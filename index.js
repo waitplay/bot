8815b4feca3068342567cc70f4a176c77c6203ff
waitplay <41130605+waitplay@users.noreply.github.com>
2018-07-12 14:33:07 +0300

    в

--- index.js
+++ index.js
@@ -5,21 +5,10 @@ const TOKEN = "585275748:AAEmqfzsXQo2MZBHdvfIK_HrAfqaHaUdJEI";
 API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN;
 
 
-async def handler(request);:
-data = await request.json();
-headers = {
-    'Content-Type': 'application/json'
-};
-message = {
-    'chat_id': data['message']['chat']['id'],
-    'text': data['message']['text']
-}
-async with aiohttp.ClientSession(loop=loop) as session:
-async with session.post(API_URL,
-    data=json.dumps(message),
-    headers=headers) as resp:
-try:
-assert resp.status === 200;
-except:
-    return web.Response(status=500);
-return web.Response(status=200);
+numb = rand.random(1,2);
+if numb == 1:
+bot.send_message(message.chat.id, "Text1");
+elif numb == 2:
+bot.send_message(message.chat.id, "Text 2");
+else:
+bot.send_message(message.chat.id, "end");
