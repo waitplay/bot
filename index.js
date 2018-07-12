6bab23bc5d4aac344abc9396d943163a68b84575
waitplay <41130605+waitplay@users.noreply.github.com>
2018-07-12 05:37:05 +0300

    ss

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const TelegramBot = require('node-telegram-bot-api');
 
 
 const TOKEN = "585275748:AAEmqfzsXQo2MZBHdvfIK_HrAfqaHaUdJEI";
+API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN;
 
 
-const bot = new TelegramBot(TOKEN, {polling: true});
-bot.on('message' , msg => {
-    return bot.sendMessage(msg.chat.id, `BOT YES or NO , says: "Hi, ${msg.from.first_name}"`);
-});
+async def handler(request);:
+data = await request.json();
+headers = {
+    'Content-Type': 'application/json'
+};
+message = {
+    'chat_id': data['message']['chat']['id'],
+    'text': data['message']['text']
+}
+async with aiohttp.ClientSession(loop=loop) as session:
+async with session.post(API_URL,
+    data=json.dumps(message),
+    headers=headers) as resp:
+try:
+assert resp.status === 200;
+except:
+    return web.Response(status=500);
+return web.Response(status=200);
