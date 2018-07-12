9856a6dfa2c3b9f1ad1a3e219e8ec2046bb0b018
waitplay <41130605+waitplay@users.noreply.github.com>
2018-07-12 14:43:57 +0300

    LLLL

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const TOKEN = "585275748:AAEmqfzsXQo2MZBHdvfIK_HrAfqaHaUdJEI";
 API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN;
 
 
-numb = rand.random(1,2)
-if numb == 1:
-bot.send_message(message.chat.id, "YES")
-elif numb == 2:
-bot.send_message(message.chat.id, "NO")
-else:
-bot.send_message(message.chat.id, "end")
+import random
+a = [];
+for i in range(1, 101):
+a.append(i);
+random.shuffle(a);
