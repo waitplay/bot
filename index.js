da8a8f1cb029e33148ca8708a346f5272b78ebe4
waitplay <41130605+waitplay@users.noreply.github.com>
2018-07-12 14:52:53 +0300

    DDDDDDD

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = "585275748:AAEmqfzsXQo2MZBHdvfIK_HrAfqaHaUdJEI";
 API_URL = 'https://api.telegram.org/bot%s/sendMessage' % TOKEN;
+# -*- coding: utf-8 -*-
+    from telegram.ext import Updater
+from telegram.ext import CommandHandler
 
-
+def start(bot, update):
 numb = rand.random(1,2);
 if numb == 1:
-bot.send_message(message.chat.id, "Text1");
-elif; numb == 2;:
-bot.send_message(message.chat.id, "Text 2");
+bot.send_message(message.chat.id, "YES");
+elif numb === 2:
+bot.send_message(message.chat.id, "NO");
 else:
 bot.send_message(message.chat.id, "end");
 
